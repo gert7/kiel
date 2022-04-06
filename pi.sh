c1bea177e9f6a844206e4bcbf35d3638ba6eca9e
gert7 <gert7@pm.me>
2022-04-06 19:55:18 +0300

    Add docker line

--- pi.sh
+++ pi.sh
@@ -1,2 +1,3 @@
-sudo apt install libpq-dev postgresql libssl-dev
+sudo apt install libpq-dev postgresql libssl-dev docker.io
+sudo docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=Europe/Tallinn -v /home/pi/homeassistant:/config --network=host ghcr.io/home-assistant/home-assistant:stable
 
