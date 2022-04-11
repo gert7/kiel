97ed41652fac5dac984b443f9526ce908d7e9051
gert7 <gert7@pm.me>
2022-04-11 20:11:50 +0300

    Python plug switcher

---
+++ kieldirect/try.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import os
+import sys
+from phue import Bridge
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+# 00:17:88:01:0b:df:92:fa-0b
+
+def set_light(b: Bridge, i: int, state: bool):
+    b.set_light(i, 'on', state)
+
+def toggle_light(b: Bridge, i):
+    is_on = b.get_light(i, 'on')
+
+    if is_on:
+        b.set_light(i, 'on', False)
+    else:
+        b.set_light(i, 'on', True)
+
+def main():
+    power_state = sys.argv[1]
+    state: Optional[bool] = None
+    if power_state == "0":
+        state = False
+    elif power_state == "1":
+        state = True
+    else:
+        print("No valid power argument supplied!")
+        print("Valid arguments are: 0 = off, 1 = on")
+        exit(1)
+
+    plug_uid = os.getenv("KIEL_PLUG_UID")
+    print(plug_uid)
+
+    b = Bridge('192.168.1.201')
+
+    b.connect()
+    api: dict = b.get_api()
+    for key, light in api["lights"].items():
+        if light['uniqueid'] == plug_uid:
+            to_i = int(key)
+            set_light(b, to_i, state)
+            break
+
+if __name__ == "__main__":
+    main()
