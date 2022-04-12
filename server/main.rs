b386c09de99e5cbc978373b327145b39abb4412e
gert7 <gert7@pm.me>
2022-04-12 21:55:24 +0300

    Kiel prints on request

--- server/main.rs
+++ server/main.rs
@@ -6,7 +6,10 @@ async fn main() {
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("service" / String)
-        .map(|name| format!("Kiel says hello, {}!", name));
+        .map(|name| {
+            println!("Kiel service request received!");
+            format!("Kiel says hello, {}!", name)
+        });
     let world = warp::path!("world" / String)
         .map(|name| format!("{}, world!", name));
     
