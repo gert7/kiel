2ada9df34ad5bdbd9413a41ff8849d53755c658e
gert7 <gert7@pm.me>
2022-04-14 09:19:29 +0300

    Clean garbage

--- install_units.sh
+++ install_units.sh
@@ -8,12 +8,8 @@ cd -
 cp systemd_units/* $USYSDIR
 
 sudo cp target/release/server /usr/local/bin/kielserver
-
-<<<<<<< HEAD
-sudo cp target/debug/server /usr/local/bin/kielserver
+sudo cp target/release/kiel /usr/local/bin/kiel
 
 sudo mkdir -p /etc/kiel.d
 sudo cp default.toml /etc/kiel.d/default.toml
 sudo cp .env /etc/kiel.d/.env
-=======
->>>>>>> 808c09b1f03f70a2ea9b8c3a1c58d670bb16b6ba
