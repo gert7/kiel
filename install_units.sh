fe6199f1c9dd20cdbbbfc2dd73bae86b4aa90a52
Gert Oja <gert7@pm.me>
2023-02-25 10:09:15 +0200

    New install_units

--- install_units.sh
+++ install_units.sh
@@ -2,41 +2,41 @@
 
 export USYSDIR=/usr/lib/systemd/system/
 
-sudo systemctl stop kiel.target
-sudo systemctl stop kielserver.service
-sudo systemctl stop kieltimer.timer
-sudo systemctl stop kieltimer.service
-sudo systemctl stop kielfetch.timer
-sudo systemctl stop kielfetch.service
-sudo systemctl stop homeguarantee.timer
-sudo systemctl stop homeguarantee.service
+systemctl stop kiel.target
+systemctl stop kielserver.service
+systemctl stop kieltimer.timer
+systemctl stop kieltimer.service
+systemctl stop kielfetch.timer
+systemctl stop kielfetch.service
+systemctl stop homeguarantee.timer
+systemctl stop homeguarantee.service
 
 cd "$HOME" || exit
 mkdir -p $USYSDIR
 cd - || exit
 cp systemd_units/* $USYSDIR
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
-sudo cp target/release/server /usr/local/bin/kielserver
-sudo cp target/release/kiel /usr/local/bin/kiel
+cp target/release/server /usr/local/bin/kielserver
+cp target/release/kiel /usr/local/bin/kiel
 
-sudo mkdir -p /etc/kiel.d
-sudo cp default.toml /etc/kiel.d/default.toml
-sudo cp .env /etc/kiel.d/.env
+mkdir -p /etc/kiel.d
+cp default.toml /etc/kiel.d/default.toml
+cp .env /etc/kiel.d/.env
 
 if [ "$1" = 0 ]
 then
   echo "Units installed. Execute 'sudo systemctl start kiel.target' to start services."
 else
   echo "Units installed."
-  sudo systemctl start kiel.target
-  sudo systemctl start homeguarantee.service
+  systemctl start kiel.target
+  systemctl start homeguarantee.service
 fi
 
 if [ "$2" = "nofetch" ]
 then
-  sudo systemctl stop kielfetch.timer
-  sudo systemctl stop kielfetch.service
+  systemctl stop kielfetch.timer
+  systemctl stop kielfetch.service
 fi
 
